2955bf441411c5e71cb2aca9eecd83c17bd6996b
devbhavsar005-ctrl <devbhavsar005@gmail.com>
2025-08-16 15:29:21 +0530

    utils

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -1,17 +1,17 @@
 import os
 import sys
-from src.exception import CustomException
+from src.exception import customException
 from src.logger import logging
 import pandas as pd
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from src.component.data_transformation import DataTransformation
+from src.component.data_transformation import DataTransformationConfig
 
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
+from src.component.model_trainer import ModelTrainerConfig
+from src.component.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
